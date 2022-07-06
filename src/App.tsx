2b0e4f72780f555dc00d08b099ad3731d6eca350
Jaroslawleszko <jaroslawleszko@gmail.com>
2022-07-06 14:11:54 +0200

    Add configuration for types from backend. Add form for adding plants.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {AddPlant} from "./components/AddPlant";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <header>
+
+            <h1>Plant Control</h1>
+            <div className="search">
+                <input type="text"/> <button>Search</button>
+            </div>
+        </header>
+        <button className="add-button">Add plant</button>
+        <AddPlant/>
+        <div className="plant-list">
+
+        </div>
+    </>
   );
 }
 
---
+++ src/components/AddPlant.tsx
@@ -0,0 +1,126 @@
+import React, {FormEvent, useState} from 'react';
+import {PlantEntity} from 'types';
+
+export const AddPlant = () => {
+
+    const [form, setForm] = useState({
+        name: '',
+        lastWatering: '',
+        wateringPeriod: 0,
+        lastFertilization: '',
+        fertilizationPeriod: 0,
+        lastDustRemoval: '',
+        quarantine: 0,
+        image: '',
+
+    });
+
+    const updateForm = (key:string, value: any) => {
+        setForm(form => ({
+            ...form,
+            [key]: value,
+        }));
+    };
+
+    const sendForm = async (e: FormEvent) => {
+        e.preventDefault();
+
+
+        try {
+            const res = await fetch(`http://localhost:3001/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(form),
+            });
+            const data: PlantEntity = await res.json();
+        } finally {
+            console.log('hh');
+        }
+    };
+
+    return <form onSubmit={sendForm}>
+        <h2>Add plant to collection</h2>
+        <p>
+            <label>
+                Name: <br/>
+                <input
+                    type="text"
+                    value={form.name}
+                    onChange={e => updateForm('name', e.target.value)}
+                />
+            </label>
+        </p>
+        <p>
+            <label>
+                Last watering: <br/>
+                <input
+                    type="date"
+                    value={form.lastWatering}
+                    onChange={e => updateForm('lastWatering', e.target.value)}
+                />
+            </label>
+        </p>
+        <p>
+            <label>
+                Watering period (days): <br/>
+                <input
+                    type="number"
+                    value={form.wateringPeriod}
+                    onChange={e => updateForm('wateringPeriod', Number(e.target.value))}
+                />
+            </label>
+        </p>
+        <p>
+            <label>
+                Last Fertilization: <br/>
+                <input
+                    type="date"
+                    value={form.lastFertilization}
+                    onChange={e => updateForm('lastFertilization', e.target.value)}
+                />
+            </label>
+        </p>
+
+        <p>
+            <label>
+                Fertilization period (days): <br/>
+                <input
+                    type="number"
+                    value={form.fertilizationPeriod}
+                    onChange={e => updateForm('fertilizationPeriod', Number(e.target.value))}
+                />
+            </label>
+        </p>
+
+        <p>
+            <label>
+                Last Dust removal: <br/>
+                <input
+                    type="date"
+                    value={form.lastDustRemoval}
+                    onChange={e => updateForm('lastDustRemoval', e.target.value)}
+                />
+            </label>
+        </p>
+        <p>
+            <label>
+                Quarantine: <br/>
+                <button type="reset" onClick={e => updateForm('quarantine', 1)}>Yes</button>
+                <button type="reset" onClick={e => updateForm('quarantine', 0)}>No</button>
+             </label>
+        </p>
+        <p>
+            <label>
+                Image (add URL): <br/>
+                <input
+                    type="url"
+                    value={form.image}
+                    onChange={e => updateForm('image', e.target.value)}
+                />
+            </label>
+        </p>
+        <button type="submit">Save</button>
+    </form>;
+};
